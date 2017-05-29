ee88fe27769d52d3fa06bb3e403c844638ddde94
Taylor Edmiston <tedmiston@gmail.com>
2017-05-29 19:31:31 -0400

    Use more docstrings

--- FixPath.py
+++ FixPath.py
@@ -1,3 +1,15 @@
+"""
+A Sublime Text 2/3 plugin for reading $PATH in Mac OS X.
+
+Depending on how environment variables are set in Mac OS X, they may not be
+recognized in GUI apps [1][2]. This package is needed to pull in user
+modifications to PATH, such as giving precedence to binaries from Homebrew
+over system binaries with the same name, eg git.
+
+[1]: https://forum.sublimetext.com/t/solved-st2-does-not-respect-system-path-in-osx-10-8/6806  # noqa
+[2]: https://stackoverflow.com/a/4567308/149428
+"""
+
 from __future__ import (absolute_import, division, print_function,
                         unicode_literals)
 
@@ -29,28 +41,35 @@ def is_sublime_v2():
 
 
 def get_sys_path():
+    """Get the $PATH environment variable from the OS.
+
+    The command to retrieve it is executed using the original environment; 
+    otherwise, our PATH changes propogate down to the shell we spawn, which
+    re-adds the system PATH, resulting in duplication.
+
+    After retrieving the PATH, we decode the byte array into a string, then
+    remove ANSI control characters [1], trailing whitespace, and colon.
+
+    [1]: http://www.commandlinefu.com/commands/view/3584/remove-color-codes-special-characters-with-sed  # noqa
+    """
     command = ("TERM=ansi CLICOLOR=\"\" SUBLIME=1 "
                "/usr/bin/login -fqpl $USER $SHELL -l -c "
                "'TERM=ansi CLICOLOR=\"\" SUBLIME=1 printf \"%s\" \"$PATH\"'")
 
-    # Execute command with original environ. Otherwise, our changes to the PATH
-    # propogate down to the shell we spawn, which re-adds the system path &
-    # returns it, leading to duplicate values.
     pipe = Popen(command, stdout=PIPE, shell=True, env=original_env)
     sys_path = pipe.stdout.read()
 
     sys_path_string = sys_path.decode('utf-8')
-    # Remove ANSI control characters (see: http://www.commandlinefu.com/commands/view/3584/remove-color-codes-special-characters-with-sed)  # noqa
     ansi_control_chars = re.compile(r'\x1B\[([0-9]{1,2}(;[0-9]{1,2})?)?[m|K]')
     sys_path_string = ansi_control_chars.sub('', sys_path_string)
-    # Decode the byte array into a string; remove trailing whitespace, colon
     sys_path_string = sys_path_string.strip().rstrip(':')
     return sys_path_string
 
 
 def fix_path():
+    """Apply the system PATH to the environment used by Sublime."""
     curr_sys_path = get_sys_path()
-    # Basic sanity check to make sure our new path is not empty
+    # Basic sanity check to ensure new PATH is not empty
     if len(curr_sys_path) < 1:
         return False
 
@@ -63,14 +82,16 @@ def fix_path():
 
 
 def plugin_loaded():
+    """Setup hooks to reload if settings change and run fix_path().
+
+    Save original environ (particularly PATH) to restore later
+    """
     global fix_path_settings, original_env
 
     fix_path_settings = sublime.load_settings('Preferences.sublime-settings')
     fix_path_settings.clear_on_change('fixpath-reload')
     fix_path_settings.add_on_change('fixpath-reload', fix_path)
 
-    # Save the original environ (particularly the original PATH) to restore
-    # later
     for key in environ:
         original_env[key] = environ[key]
 
@@ -78,10 +99,12 @@ def plugin_loaded():
 
 
 def plugin_unloaded():
+    """Reset PATH to original value.
+
+    This prevents it from becoming duplicated when this plugin is reloaded.
+    """
     global fix_path_settings
 
-    # When we unload, reset PATH to original value. Otherwise, reloads of this
-    # plugin will cause the PATH to be duplicated.
     environ['PATH'] = original_env['PATH']
 
     fix_path_settings.clear_on_change('fixpath-reload')
